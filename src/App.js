4ca6852c642fa4850ec2a76a3d2041f931583ba4
mudzhasbit <mudzakkir.aziz@gmail.com>
2022-05-06 11:41:56 +0800

    added LoginForm

--- src/App.js
+++ src/App.js
@@ -1,14 +1,44 @@
-import React from "react"
+import React, { useState } from "react";
+import Dashboard from "./john-components/Dashboard";
+import LoginForm from './mudz-components/LoginForm'
 
 function App() {
+
+  const adminUser = {
+    email: "admin@admin.com",
+    password: "admin123"
+  }
+
+  const [user, setUser] = useState({name:"", email:""});
+  const [error, setError] = useState("");
+
+  const Login = details => {
+    console.log(details);
+    if (details.email == adminUser.email && details.password == adminUser.password){
+      console.log("Logged In")
+      setUser({
+        name: details.name,
+        email: details.email
+      });
+    } else {
+      console.log("Details Do Not Match!")
+    }
+  }
+
+  const Logout = () => {
+    setUser({name: "", email: ""});
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        
-        <a>
-          Learn React
-        </a>
-      </header>
+      {(user.email != "") ? (
+        <div className="welcome">
+          <h2>Welcom, <span>{user.name}</span></h2>
+          <button>onClick={Logout}</button>
+        </div>
+      ) : (
+        <LoginForm />
+      )}
     </div>
   );
 }
